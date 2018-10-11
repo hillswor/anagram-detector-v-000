8c8a4bf0e329e1dbdee5d22d24662534d6e0d751
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-10-11 18:32:48 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagram)
+    @word.split(" ")
+  end
+
+end
+binding.pry
+
+def match(word_array)
+  word_array.select do |word|
+    word.split("")
+  end
+end
